5190f28c564a01b1eefdbf5b4f31fd08cbf2d578
mel-murphy <cumurphy*pdx.edu>
2021-05-10 21:31:18 -0700

    command line and file I/O

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+// libraries and includes
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// definitions
+#define FUNCTIONAL 	0	// simulator modes
+#define TIMING		1
+#define FORWARDING 	2
+
+#define OFF 		0	// general definitions
+#define ON		1
+
+// function prototypes
+FILE* openInputFile(const char* inputFile);
+FILE* openOutputFile(const char* outputFile);
+void closeFile(FILE* inputFile);
+
+#endif
